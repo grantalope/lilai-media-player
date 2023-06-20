0ad6594f736d2f940c4a00ed45fffbea597de900
grantalope <64297836+grantalope@users.noreply.github.com>
2023-06-20 15:03:08 -0700

    Setup Express server for Azure

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var app = express();
 
 app.use(serveStatic('public', {'index': ['index.html']}));
 app.listen(process.env.PORT || 5000);
+
+console.log('Server started on port ' + (process.env.PORT || 5000));
