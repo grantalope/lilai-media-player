dfab99267f8316685deb76b2be75c7d2568ce98a
grantalope <64297836+grantalope@users.noreply.github.com>
2023-06-22 08:35:54 -0700

    changed radius to 10px

--- src/App.svelte
+++ src/App.svelte
@@ -42,4 +42,4 @@
   {/each}
 </div>
 
-<iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/28hLyn3dYE70ZOKye24IhI?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<iframe style="border-radius:10px" src="https://open.spotify.com/embed/playlist/28hLyn3dYE70ZOKye24IhI?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
